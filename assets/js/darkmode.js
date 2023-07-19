cf2115b1eb08d407dcc20a012bb5db727b80744a
Jérémie Gaven <jeremie+gitlab@gaven.me>
2023-07-19 11:01:37 +0200

    fix a bit darkmode ugly code

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,26 +1,26 @@
-var moon =
-  '<svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 18a6 6 0 100-12 6 6 0 000 12zM22 12h1M12 2V1M12 23v-1M20 20l-1-1M20 4l-1 1M4 20l1-1M4 4l1 1M1 12h1" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-var sun =
-  '<svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 11.507a9.493 9.493 0 0018 4.219c-8.507 0-12.726-4.22-12.726-12.726A9.494 9.494 0 003 11.507z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+const DARK_STRING = "dark";
+const LIGHT_STRING = "light";
 
-let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
+let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: " + DARK_STRING + ")");
 let theme = sessionStorage.getItem("theme");
+let button = document.getElementById("theme-toggle");
 
 if (systemInitiatedDark.matches) {
-  document.getElementById("theme-toggle").innerHTML = sun;
+  button.innerHTML = sunSvg();
 } else {
-  document.getElementById("theme-toggle").innerHTML = moon;
+  button.innerHTML = moonSvg();
 }
 
 function prefersColorTest(systemInitiatedDark) {
+  let button = document.getElementById("theme-toggle");
   if (systemInitiatedDark.matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    document.getElementById("theme-toggle").innerHTML = sun;
-    sessionStorage.setItem("theme", "");
+    setDarkDataTheme();
+    button.innerHTML = sunSvg();
+    setSessionStorageEmpty();
   } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    document.getElementById("theme-toggle").innerHTML = moon;
-    sessionStorage.setItem("theme", "");
+    setLightDataTheme()
+    button.innerHTML = moonSvg();
+    setSessionStorageEmpty();
   }
 }
 systemInitiatedDark.addEventListener("change", function (event) {
@@ -28,32 +28,61 @@ systemInitiatedDark.addEventListener("change", function (event) {
 });
 
 function modeSwitcher() {
+  let button = document.getElementById("theme-toggle");
   let theme = sessionStorage.getItem("theme");
-  if (theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "light");
-    sessionStorage.setItem("theme", "light");
-    document.getElementById("theme-toggle").innerHTML = moon;
-  } else if (theme === "light") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    sessionStorage.setItem("theme", "dark");
-    document.getElementById("theme-toggle").innerHTML = sun;
+  if (theme === DARK_STRING) {
+    setLightDataTheme()
+    setSessionStorageLight();
+    button.innerHTML = moonSvg();
+  } else if (theme === LIGHT_STRING) {
+    setDarkDataTheme();
+    setSessionStorageDark();
+    button.innerHTML = sunSvg();
   } else if (systemInitiatedDark.matches) {
-    document.documentElement.setAttribute("data-theme", "light");
-    sessionStorage.setItem("theme", "light");
-    document.getElementById("theme-toggle").innerHTML = moon;
+    setLightDataTheme()
+    setSessionStorageLight();
+    button.innerHTML = moonSvg();
   } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    sessionStorage.setItem("theme", "dark");
-    document.getElementById("theme-toggle").innerHTML = sun;
+    setDarkDataTheme();
+    setSessionStorageDark();
+    button.innerHTML = sunSvg();
   }
 }
 
-if (theme === "dark") {
-  document.documentElement.setAttribute("data-theme", "dark");
-  sessionStorage.setItem("theme", "dark");
-  document.getElementById("theme-toggle").innerHTML = sun;
-} else if (theme === "light") {
-  document.documentElement.setAttribute("data-theme", "light");
-  sessionStorage.setItem("theme", "light");
-  document.getElementById("theme-toggle").innerHTML = moon;
+function setDarkDataTheme() {
+  document.documentElement.setAttribute("data-theme", DARK_STRING);
+}
+
+function setLightDataTheme() {
+  document.documentElement.setAttribute("data-theme", LIGHT_STRING);
+}
+
+function setSessionStorageLight() {
+  sessionStorage.setItem("theme", LIGHT_STRING);
+}
+
+function setSessionStorageDark() {
+  sessionStorage.setItem("theme", DARK_STRING);
+}
+
+function setSessionStorageEmpty() {
+  sessionStorage.setItem("theme", "");
+}
+
+function moonSvg() {
+  return '<svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 18a6 6 0 100-12 6 6 0 000 12zM22 12h1M12 2V1M12 23v-1M20 20l-1-1M20 4l-1 1M4 20l1-1M4 4l1 1M1 12h1" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+}
+
+function sunSvg() {
+  return '<svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 11.507a9.493 9.493 0 0018 4.219c-8.507 0-12.726-4.22-12.726-12.726A9.494 9.494 0 003 11.507z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+}
+
+if (theme === DARK_STRING) {
+  setDarkDataTheme();
+  setSessionStorageDark();
+  button.innerHTML = sunSvg();
+} else if (theme === LIGHT_STRING) {
+  setLightDataTheme()
+  setSessionStorageLight();
+  button.innerHTML = moonSvg();
 }
